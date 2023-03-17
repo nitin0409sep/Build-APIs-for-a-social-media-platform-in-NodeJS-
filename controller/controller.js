a05d5921805a6bbf54328bb7b247bc1f9c5f6003
Nitin Verma <nitin5september@gmail.com>
2023-03-17 21:50:45 +0530

    Authentication Done

--- controller/controller.js
+++ controller/controller.js
@@ -17,27 +17,27 @@ const user = require('../routes/User/user');
 const posts = require('../routes/Post/post');
 
 // Home
-route.get('/api/user', home.getUserHome);                   // GET User Home
-route.get('/api/posts', home.getPostHome);                  // GET Post Home
+route.get('/api/user', home.getUserHome);                          // GET User Home
+route.get('/api/posts', home.getPostHome);                         // GET Post Home
 
 // User - Register, Login, Update, Delete, Follow, Unfollow
-route.post('/api/registerUser', user.createUser);           // Create User
-route.get('/api/authenticate', user.authenticate);          // Authenticate User
-route.get('/api/user/:id', user.findUserById);              // Get User By Id
-route.post('/api/updateUser/:id', user.updateUser);          // Update User
-route.post('/api/follow/:id', user.followUser);              // Follow User
-route.post('/api/unfollow/:id', user.unFollowUser);          // Unfollow User
-route.delete('/api/deleteUser/:id', user.deleteUser);       // Delete User
+route.post('/api/registerUser', user.createUser);                  // Create User
+route.get('/api/authenticate', user.authenticate);                 // Authenticate User
+route.get('/api/user/:id', auth, user.findUserById);               // Get User By Id
+route.post('/api/updateUser/:id', auth, user.updateUser);          // Update User
+route.post('/api/follow/:id', auth, user.followUser);              // Follow User
+route.post('/api/unfollow/:id', auth, user.unFollowUser);          // Unfollow User
+route.delete('/api/deleteUser/:id', auth, user.deleteUser);        // Delete User
 
 // Post - Create, Get, Update, Delete, Like, Dislike, Comment
-route.post('/api/posts', posts.createPost);                 // Create User
-route.get('/api/posts/:id', posts.getPost)                  // Get Post
-route.get('/api/all_posts', posts.getAllPost)               // Get Tmeline Posts
-route.post('/api/updatePost/:id', posts.updatePost)          // Update Post
-route.post('/api/like/:id', posts.likePost)                  // Like Post
-route.post('/api/unlike/:id', posts.dislikePost)             // Dislike Post
-route.post('/api/comment/:id', posts.commentPost)            // Comment Post
-route.delete('/api/posts/:id', posts.deletePost)            // Delete Post
+route.post('/api/posts', auth, posts.createPost);                  // Create User
+route.get('/api/posts/:id', auth, posts.getPost)                   // Get Post
+route.get('/api/all_posts', auth, posts.getAllPost)                // Get Tmeline Posts
+route.post('/api/updatePost/:id', auth, posts.updatePost)          // Update Post
+route.post('/api/like/:id', auth, posts.likePost)                  // Like Post
+route.post('/api/unlike/:id', auth, posts.dislikePost)             // Dislike Post
+route.post('/api/comment/:id', auth, posts.commentPost)            // Comment Post
+route.delete('/api/posts/:id', auth, posts.deletePost)             // Delete Post
 
 // Export Module
 module.exports = route;
